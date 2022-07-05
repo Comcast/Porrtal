b7042221523888ab629167839fdbf6f3928eb5d6
datumgeek <datumgeek@yahoo.com>
2022-07-05 05:32:27 -0600

    remove unused shell-layout component

--- libs/porrtal/shell/src/index.ts
+++ libs/porrtal/shell/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/shell-layout-splitter/shell-layout-splitter';
-export * from './lib/shell-layout/shell-layout';
 export * from './lib/shell-state/shell-state';
 export * from './lib/view-host/view-host';
 export * from './lib/view-stack-props';
