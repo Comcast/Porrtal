d25256bb8acb44236fd7a445c2487359cca8c248
datumgeek <datumgeek@yahoo.com>
2022-07-07 14:14:16 -0600

    feat(@porrtal/shell-blueprint): add search input

--- libs/porrtal/shell-blueprint/src/lib/search/search.tsx
+++ libs/porrtal/shell-blueprint/src/lib/search/search.tsx
@@ -1,3 +1,4 @@
+import { Icon, InputGroup } from '@blueprintjs/core';
 import styles from './search.module.scss';
 
 /* eslint-disable-next-line */
@@ -6,7 +7,14 @@ export interface SearchProps {}
 export function Search(props: SearchProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Search!</h1>
+      <InputGroup className={styles['search-input']}
+        leftElement={
+          <Icon
+            onClick={(evt) => alert('search it !! :)')}
+            icon="search"
+          ></Icon>
+        }
+      ></InputGroup>
     </div>
   );
 }
