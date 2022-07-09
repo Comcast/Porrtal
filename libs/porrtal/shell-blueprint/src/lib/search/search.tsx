7c2ae6ff7f69c652fbd49b0caedb1dec840eed8d
datumgeek <datumgeek@yahoo.com>
2022-07-09 04:42:08 -0600

    feat(@porrtal/shell-blueprint): search popup size working

--- libs/porrtal/shell-blueprint/src/lib/search/search.tsx
+++ libs/porrtal/shell-blueprint/src/lib/search/search.tsx
@@ -1,22 +1,69 @@
 import { Icon, InputGroup } from '@blueprintjs/core';
+import { Popover2 } from '@blueprintjs/popover2';
+import { useShellComponents, useShellState } from '@porrtal/shell';
+import { useEffect, useRef, useState } from 'react';
 import styles from './search.module.scss';
 
 /* eslint-disable-next-line */
 export interface SearchProps {}
 
 export function Search(props: SearchProps) {
-  return (
-    <div className={styles['container']}>
-      <InputGroup className={styles['search-input']}
-        leftElement={
-          <Icon
-            onClick={(evt) => alert('search it !! :)')}
-            icon="search"
-          ></Icon>
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const shellComponents = useShellComponents();
+  const shellState = useShellState();
+  const [dialogBoundary, setDialogBoundary] = useState<HTMLElement>();
+  const divRef = useRef<HTMLDivElement>(null);
+  useEffect(() => setDialogBoundary(document.body), []);
+  console.log('input group ref', divRef?.current?.offsetLeft);
+
+  if (shellComponents && typeof window !== 'undefined' && window) {
+    return (
+      // <div className={styles['container']}>
+      <Popover2
+        // className={styles['search-dialog']}
+        // boundary={dialogBoundary}
+        // portalContainer={dialogBoundary}
+        // targetTagName='div'
+        // popoverClassName={styles['search-popover']}
+        // portalClassName={styles['search-portal']}
+        isOpen={isDialogOpen}
+        onClose={(evt) => setIsDialogOpen(false)}
+        content={
+          <div
+            className={styles['search-container']}
+            style={{
+              width: divRef.current
+                ? `${divRef.current.offsetLeft - 50}px`
+                : '700px',
+              height: dialogBoundary
+                ? `${dialogBoundary.offsetHeight - 50}px`
+                : '700px',
+            }}
+          >
+            <shellComponents.ViewStack
+              pane={shellState.panes.search}
+            ></shellComponents.ViewStack>
+          </div>
         }
-      ></InputGroup>
-    </div>
-  );
+        placement="left-start"
+      >
+        <div ref={divRef}>
+          <InputGroup
+            className={styles['search-input']}
+            leftElement={
+              <Icon
+                onClick={(evt) => setIsDialogOpen(true)}
+                icon="search"
+              ></Icon>
+            }
+          ></InputGroup>
+        </div>
+      </Popover2>
+      // </div>
+    );
+  } else {
+    return <div></div>;
+  }
 }
 
 export default Search;
