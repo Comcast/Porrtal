b4f1dfc9f8256a44ba18fe8cbf442f373c53ac6c
datumgeek <datumgeek@yahoo.com>
2022-07-24 05:54:09 -0600

    feat(@porrtal/shell-material): entity menu working

--- libs/porrtal/shell-material/src/lib/entity-menu/entity-menu.tsx
+++ libs/porrtal/shell-material/src/lib/entity-menu/entity-menu.tsx
@@ -1,12 +1,67 @@
-import { Button } from '@mui/material';
-import { EntityMenuProps } from '@porrtal/shell';
-import styles from './entity-menu.module.scss';
+import { Icon, Menu } from '@mui/material';
+import {
+  EntityMenuProps,
+  useShellDispatch,
+  useShellState,
+} from '@porrtal/shell';
+import { IconMenuItem } from 'mui-nested-menu';
+import { useState, MouseEvent } from 'react';
 
 export function EntityMenu(props: EntityMenuProps) {
+  const shellState = useShellState();
+  const dispatch = useShellDispatch();
+
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
-    <Button onClick={(evt) => alert(`show menu`)}>
-      {props.children}
-    </Button>
+    <>
+      <span
+        id="entity-menu-trigger"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        {props.children}
+      </span>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {shellState.views
+          .filter((view) => view.entityType === props.entityType)
+          .map((view) => (
+            <IconMenuItem
+              key={view.viewId}
+              leftIcon={<Icon>{view.displayIconTemplate}</Icon>}
+              label={view.displayTextTemplate}
+              onClick={() => {
+                dispatch({
+                  type: 'launchView',
+                  viewId: view.viewId,
+                  state: props.state,
+                });
+                setAnchorEl(null);
+              }}
+            />
+          ))}
+      </Menu>
+    </>
   );
 }
 
