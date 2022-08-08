9dbab824505560902018ddf652af27660818e67f
datumgeek <datumgeek@yahoo.com>
2022-08-08 07:10:33 -0600

    feat(@porrtal/a-shell-material): icons on tabs working

--- libs/porrtal/a/shell-material/src/lib/view-stack/view-stack.component.ts
+++ libs/porrtal/a/shell-material/src/lib/view-stack/view-stack.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Pane } from '@porrtal/a-api';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { ViewHostComponent } from '@porrtal/a-shell';
 
 @Component({
   selector: 'porrtal-workspace-view-stack',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTabsModule, MatIconModule, ViewHostComponent],
   templateUrl: './view-stack.component.html',
   styleUrls: ['./view-stack.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
