d234815f9b134384a31995497899df4d7b239820
datumgeek <datumgeek@yahoo.com>
2022-08-21 06:07:28 -0600

    feat(@porrtal/a-shell-material): search text and button

--- libs/porrtal/a/shell-material/src/lib/search/search.component.ts
+++ libs/porrtal/a/shell-material/src/lib/search/search.component.ts
@@ -1,14 +1,28 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'porrtal-search',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchComponent {
   constructor() {}
+
+  doSearch() {
+    alert('search...');
+  }
 }
