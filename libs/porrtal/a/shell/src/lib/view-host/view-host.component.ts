11657d1c97c0b5355f381c6b42967f7fe8749934
datumgeek <datumgeek@yahoo.com>
2022-08-28 06:26:16 -0500

    feat(@porrtal/a-shell): create scrolling containers in view host

--- libs/porrtal/a/shell/src/lib/view-host/view-host.component.ts
+++ libs/porrtal/a/shell/src/lib/view-host/view-host.component.ts
@@ -1,4 +1,12 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit, ViewContainerRef } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewComponentProps, ViewState } from '@porrtal/a-api';
 
@@ -15,23 +23,35 @@ export class ViewHostComponent {
   @Input() public set viewState(value: ViewState) {
     this._viewState = value;
     this.loadViewStateComponent();
-  };
+  }
   public get viewState() {
     return this._viewState;
+  }
+
+  private _containerRef!: ViewContainerRef;
+  @ViewChild('containerRef', { read: ViewContainerRef })
+  public set containerRef(value: ViewContainerRef) {
+    this._containerRef = value;
+    this.loadViewStateComponent();
+  }
+  public get containerRef() {
+    return this._containerRef;
   }
 
   constructor(private viewContainerRef: ViewContainerRef) {}
 
   async loadViewStateComponent() {
-    console.log('load view state component.');
-    this.viewContainerRef.clear();
-    const component = await this.viewState.componentImport()
-    if (component) {
-      const comp = this.viewContainerRef.createComponent<ViewComponentProps>(component);
-      // comp.instance.viewState = this._viewState;
-      comp.setInput('viewState', this._viewState);
-      comp.changeDetectorRef.markForCheck();
-      comp.changeDetectorRef.detectChanges();
+    if (this.viewState && this.containerRef) {
+      this.containerRef.clear();
+      const component = await this.viewState.componentImport();
+      if (component) {
+        const comp =
+          this.containerRef.createComponent<ViewComponentProps>(component);
+
+        comp.setInput('viewState', this._viewState);
+        comp.changeDetectorRef.markForCheck();
+        comp.changeDetectorRef.detectChanges();
+      }
     }
   }
 }
