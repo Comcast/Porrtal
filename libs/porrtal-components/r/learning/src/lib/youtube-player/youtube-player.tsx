fda645000f18fd6d57efcfb9759f436c9542e409
datumgeek <datumgeek@yahoo.com>
2022-09-29 10:53:01 -0500

    feat(@porrtal-components/r-learning): youtube sizing working

--- libs/porrtal-components/r/learning/src/lib/youtube-player/youtube-player.tsx
+++ libs/porrtal-components/r/learning/src/lib/youtube-player/youtube-player.tsx
@@ -1,10 +1,18 @@
 import { ViewComponentProps } from '@porrtal/r-api';
 import YouTube from 'react-youtube';
+import useDimensions from 'react-cool-dimensions';
 import styles from './youtube-player.module.scss';
 
 export function YoutubePlayer(props: ViewComponentProps) {
+  const { observe, width, height } = useDimensions<HTMLDivElement>({});
   return (
-    <YouTube videoId={(props.viewState?.state?.['videoId'] as string)}></YouTube>
+    <div className={styles['container']} ref={observe}>
+      {width},{height}
+      <YouTube
+        opts={{width, height}}
+        videoId={props.viewState?.state?.['videoId'] as string}
+      ></YouTube>
+    </div>
   );
 }
 
