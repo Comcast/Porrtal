e7381a8d324f18413b909e8800874d235e1f8567
datumgeek <datumgeek@yahoo.com>
2022-10-11 12:16:53 -0500

    feat(a-porrtal-io-app): rough in angular app

---
+++ apps/a/porrtal-io-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'porrtal-workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'a-porrtal-io-app';
+}
