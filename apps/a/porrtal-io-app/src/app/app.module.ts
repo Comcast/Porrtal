4ff7352477ba896d79e6241e04aa13fe4e401aa8
datumgeek <datumgeek@yahoo.com>
2022-10-13 03:40:15 -0500

    feat(n-porrtal-io-app, a-porrtal-io-app): rough in angular/samples and react/samples routes

--- apps/a/porrtal-io-app/src/app/app.module.ts
+++ apps/a/porrtal-io-app/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { QuickStartComponent } from './quick-start/quick-start.component';
 import { InsidePorrtalComponent } from './inside-porrtal/inside-porrtal.component';
+import { SamplesComponent } from './samples/samples.component';
 
 @NgModule({
-  declarations: [AppComponent, QuickStartComponent, InsidePorrtalComponent],
+  declarations: [
+    AppComponent,
+    QuickStartComponent,
+    InsidePorrtalComponent,
+    SamplesComponent,
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(
@@ -15,6 +21,10 @@ import { InsidePorrtalComponent } from './inside-porrtal/inside-porrtal.componen
         {
           path: 'quick-start',
           component: QuickStartComponent,
+        },
+        {
+          path: 'samples',
+          component: SamplesComponent,
         },
         {
           path: 'inside-porrtal',
---
+++ apps/a/porrtal-io-app/src/app/samples/samples.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'porrtal-workspace-samples',
+  templateUrl: './samples.component.html',
+  styleUrls: ['./samples.component.scss'],
+})
+export class SamplesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
