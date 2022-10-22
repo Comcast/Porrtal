142a60327a589b5e6fa43a57ba6a40d10ddd409c
datumgeek <datumgeek@yahoo.com>
2022-10-22 05:37:21 -0600

    feat(@porrtal/r-shell-material): add menu to banner component

--- libs/porrtal/r/shell-material/src/lib/banner/banner.tsx
+++ libs/porrtal/r/shell-material/src/lib/banner/banner.tsx
@@ -1,5 +1,7 @@
-import { Icon } from '@mui/material';
+import { Icon, Typography, Menu } from '@mui/material';
+import { IconMenuItem } from 'mui-nested-menu';
 import { BannerProps } from '@porrtal/r-shell';
+import { ReactNode, useState, MouseEvent } from 'react';
 import styles from './banner.module.scss';
 
 export function Banner(props: BannerProps) {
@@ -9,8 +11,88 @@ export function Banner(props: BannerProps) {
       {props.bannerData?.displayIcon && (
         <Icon>{props.bannerData.displayIcon}</Icon>
       )}
-      {props.bannerData?.displayText}
+      &nbsp;
+      <BannerMenu {...props}>
+        <Typography variant="h5" gutterBottom={false}>
+          {props.bannerData?.displayText}
+          {props.bannerData?.childData &&
+            props.bannerData.childData.length > 0 && (
+              <Icon className={styles['down-icon']}>arrow_drop_down</Icon>
+            )}
+        </Typography>
+      </BannerMenu>
+      <span></span>
     </div>
+  );
+}
+
+function BannerMenu(props: BannerProps & { children: ReactNode | undefined }) {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
+  if (!props.children || !props.bannerData) {
+    return null;
+  }
+
+  if (!props.bannerData.childData || props.bannerData.childData.length < 1) {
+    return <>props.children</>;
+  }
+
+  return (
+    <>
+      <span
+        id="banner-menu-trigger"
+        aria-controls={open ? 'banner-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+      >
+        {props.children}
+      </span>
+      <Menu
+        id="banner-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {props.bannerData.childData.map((menuItem) => (
+          <IconMenuItem
+            key={menuItem.displayText}
+            leftIcon={<Icon>{menuItem.displayIcon}</Icon>}
+            label={menuItem.displayText}
+            rightIcon={
+              <Icon
+                className={styles['popout-icon']}
+                onClick={(evt) => {
+                  window.open(menuItem.targetUrl);
+                  evt.stopPropagation();
+                }}
+              >
+                open_in_new
+              </Icon>
+            }
+            onClick={() => {
+              if (menuItem.targetUrl) {
+                window.location.href = menuItem.targetUrl;
+              }
+              setAnchorEl(null);
+            }}
+          />
+        ))}
+      </Menu>
+    </>
   );
 }
 
