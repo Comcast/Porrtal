529d359f9cfd3e62d2be78b523c8138c65293ef2
datumgeek <datumgeek@yahoo.com>
2022-11-20 04:33:43 -0700

    chore(@porrtal/r-split): porrtal/r --> porrtal

--- libs/porrtal/r-split/jest.config.ts
+++ libs/porrtal/r-split/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'porrtal-r-split',
-  preset: '../../../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,5 @@ export default {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/porrtal/r/split',
+  coverageDirectory: '../../../coverage/libs/porrtal/r-split',
 };
