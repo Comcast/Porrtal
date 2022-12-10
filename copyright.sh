bed2aa88e5af85926742d686561da8be22f663b8
cgraha217 <charles_graham@comcast.com>
2022-12-10 05:32:00 -0700

    fix(workspace): define COPYRIGHTLEN for copyright script

--- copyright.sh
+++ copyright.sh
@@ -1,1 +1,2 @@
+export COPYRIGHTLEN=13
 find . -type d -name "node_modules" -prune -o -type d -name "dist" -prune -o -type d -name "**/next-env.d.ts" -prune -o -name "*.ts" -print0 | xargs -0 ./copyrightadd.sh
