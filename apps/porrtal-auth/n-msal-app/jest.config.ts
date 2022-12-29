53a022a5149d2d7f0c436ae0629caa28028ef83c
cgraha217 <charles_graham@comcast.com>
2022-12-29 13:26:05 -0700

    feat(porrtal-auth-n-*): rough in auth0, keycloak, msal, and strapi nextjs porrtal-auth apps

---
+++ apps/porrtal-auth/n-msal-app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'porrtal-auth-n-msal-app',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/apps/porrtal-auth/n-msal-app',
+};
