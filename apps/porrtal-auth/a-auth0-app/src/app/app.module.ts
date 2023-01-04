e1d768d4f74bfd07312c623b64b3c3afae12dc48
cgraha217 <charles_graham@comcast.com>
2023-01-04 03:42:47 -0700

    chore(porrtal-auth-a-auth0-app): remove unused boilerplate code

--- apps/porrtal-auth/a-auth0-app/src/app/app.module.ts
+++ apps/porrtal-auth/a-auth0-app/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { ShellLayoutComponent } from '@porrtal/a-shell-material';
