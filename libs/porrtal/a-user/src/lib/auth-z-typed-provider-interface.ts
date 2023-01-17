f1ca2903b0edc01297bbc3b9c98b38e3943192f9
cgraha217 <charles_graham@comcast.com>
2023-01-17 05:23:26 -0700

    feat(@porrtal/a-user): add auth z types, move user-login, increase build limits

--- libs/porrtal/a-user/src/lib/auth-z-typed-provider-interface.ts
+++ libs/porrtal/a-user/src/lib/auth-z-typed-provider-interface.ts
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright 2022 Comcast Cable Communications Management, LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,13 +12,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-export * from './lib/view';
-export * from './lib/view-state';
-export * from './lib/pane';
-export * from './lib/view-component-function';
-export * from './lib/view-component-props';
-export * from './lib/logger-entry';
-export * from './lib/view-component-modules';
-export * from './lib/view-launch';
-export * from './lib/porrtal-menu-item';
-export * from './lib/deep-links';
+import { InjectionToken } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type AuthZTypedProviderInterface<TConfig, TProps> = {
+  name: string;
+};
