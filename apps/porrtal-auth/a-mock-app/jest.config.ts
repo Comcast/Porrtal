9674dcfe1e78b696b9c538b5fdb9fcb85f3746be
Graham, Mike <Charles_Graham@comcast.com>
2023-03-04 04:11:41 -0700

    chore(workspace): upgrade Nx from v15.3.3 to v15.8.5

--- apps/porrtal-auth/a-mock-app/jest.config.ts
+++ apps/porrtal-auth/a-mock-app/jest.config.ts
@@ -17,15 +17,16 @@ export default {
   displayName: 'porrtal-auth-a-mock-app',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../coverage/apps/porrtal-auth/a-mock-app',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
--- libs/porrtal/a-user-auth-z/jest.config.ts
+++ libs/porrtal/a-user-auth-z/jest.config.ts
@@ -17,15 +17,16 @@ export default {
   displayName: 'porrtal-a-user-auth-z',
   preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
+  globals: {},
   coverageDirectory: '../../../coverage/libs/porrtal/a-user-auth-z',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
