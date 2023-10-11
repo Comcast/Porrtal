8b7074c9babf7fc933e40a282b66673beb8a17f7
Graham, Mike <Charles_Graham@comcast.com>
2023-10-11 03:36:45 -0700

    chore(workspace): update copyright to 2023

--- libs/porrtal/r-shell-blueprint/src/index.ts
+++ libs/porrtal/r-shell-blueprint/src/index.ts
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2022 Comcast Cable Communications Management, LLC
+Copyright 2023 Comcast Cable Communications Management, LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,20 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 export * from './lib/banner-menu-inline/banner-menu-inline';
-export * from './lib/banner-menu-bar/banner-menu-bar'; /*
-Copyright 2022 Comcast Cable Communications Management, LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+export * from './lib/banner-menu-bar/banner-menu-bar';
 export * from './lib/banner/banner';
 export * from './lib/logger-messages/logger-messages';
 export * from './lib/entity-menu/entity-menu';
