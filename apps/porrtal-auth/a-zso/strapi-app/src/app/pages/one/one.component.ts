2c1c48c5d7e094a8125a45b435d28c7f8bbc5f6e
Graham, Mike <Charles_Graham@comcast.com>
2023-11-23 06:26:14 -0700

    feat(porrtal-auth-a-zso-strapi-app): basic porrtal working

--- apps/porrtal-auth/a-zso/strapi-app/src/app/pages/one/one.component.ts
+++ apps/porrtal-auth/a-zso/strapi-app/src/app/pages/one/one.component.ts
@@ -1,11 +1,73 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { View } from '@porrtal/a-api';
+import { BannerData, ShellStateService } from '@porrtal/a-shell';
+import { ShellLayoutComponent } from '@porrtal/a-shell-material';
 
 @Component({
   selector: 'app-one',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ShellLayoutComponent],
   templateUrl: './one.component.html',
   styleUrls: ['./one.component.scss'],
 })
-export class OneComponent {}
+export class OneComponent {
+  porrtalViews: View[] = [
+    {
+      key: 'Nav',
+      launchAtStartup: true,
+      displayText: 'Nav',
+      paneType: 'nav',
+      displayIcon: 'updated',
+      componentName: 'NavComponent',
+      componentModule: () => import('../../views/nav/nav.component'),
+    },
+    {
+      key: 'Main1',
+      launchAtStartup: true,
+      displayText: 'Main1',
+      paneType: 'main',
+      displayIcon: 'updated',
+      componentName: 'Main1Component',
+      componentModule: () => import('../../views/main1/main1.component'),
+    },
+    {
+      key: 'Main2',
+      launchAtStartup: true,
+      displayText: 'Main2',
+      paneType: 'main',
+      displayIcon: 'updated',
+      componentName: 'Main2Component',
+      componentModule: () => import('../../views/main2/main2.component'),
+    },
+  ];
+  
+  porrtalBanner: BannerData = {
+    // uncomment and edit this code if you would like a banner image
+    // bannerImageUrl: './assets/my-banner.png',
+    // bannerImageHeight: 63,
+    // bannerImageWidth: 500,
+    displayText: 'My App - One',
+    displayIcon: 'cyclone',
+    childData: [
+      {
+        displayText: 'My App - Two',
+        displayIcon: 'cyclone',
+        targetUrl: '/two',
+      },
+    ],
+  };
+
+  constructor(public shellStateService: ShellStateService) {
+    this.porrtalViews.forEach((view) =>
+      shellStateService.dispatch({
+        type: 'registerView',
+        view,
+      })
+    );
+
+    shellStateService.dispatch({
+      type: 'launchStartupViews',
+    });
+  }
+}
--- apps/porrtal-auth/a-zso/strapi-app/src/app/pages/two/two.component.ts
+++ apps/porrtal-auth/a-zso/strapi-app/src/app/pages/two/two.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { View } from '@porrtal/a-api';
+import { BannerData, ShellStateService } from '@porrtal/a-shell';
+import { ShellLayoutComponent } from '@porrtal/a-shell-material';
 
 @Component({
   selector: 'app-two',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ShellLayoutComponent],
   templateUrl: './two.component.html',
   styleUrls: ['./two.component.scss'],
 })
-export class TwoComponent {}
+export class TwoComponent {
+  porrtalViews: View[] = [
+    {
+      key: 'Main2',
+      launchAtStartup: true,
+      displayText: 'Main2',
+      paneType: 'main',
+      displayIcon: 'updated',
+      componentName: 'Main2Component',
+      componentModule: () => import('../../views/main2/main2.component'),
+    },
+  ];
+  
+  porrtalBanner: BannerData = {
+    // uncomment and edit this code if you would like a banner image
+    // bannerImageUrl: './assets/my-banner.png',
+    // bannerImageHeight: 63,
+    // bannerImageWidth: 500,
+    displayText: 'My App - Two',
+    displayIcon: 'cyclone',
+    childData: [
+      {
+        displayText: 'My App - One',
+        displayIcon: 'cyclone',
+        targetUrl: '/one',
+      },
+    ],
+  };
+
+  constructor(public shellStateService: ShellStateService) {
+    this.porrtalViews.forEach((view) =>
+      shellStateService.dispatch({
+        type: 'registerView',
+        view,
+      })
+    );
+
+    shellStateService.dispatch({
+      type: 'launchStartupViews',
+    });
+  }
+}
