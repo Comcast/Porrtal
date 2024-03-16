5fb2411275d43c1e700ceaea07ad83753ce03157
Graham, Mike <Charles_Graham@comcast.com>
2024-03-16 02:22:13 -0600

    feat(porrtal-auth-a-msal-stand-alone-app): rough in pages and views

---
+++ apps/porrtal-auth/a-msal-stand-alone-app/src/app/msal-config-example.ts
@@ -0,0 +1,43 @@
+﻿/*
+Copyright 2023 Comcast Cable Communications Management, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Update MSAL Configuration
+//   1. Edit clientId on line 26
+//   2. Edit authority on line 28
+//   3. Edit redirectUri on line 29
+//   3. Rename this file to "msal-config.ts"
+
+import { BrowserCacheLocation, Configuration, LogLevel } from '@azure/msal-browser';
+
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: '<my client id>',
+    authority:
+      'https://login.microsoftonline.com/<my org id>/',
+    redirectUri: 'http://localhost:4200',
+  },
+  cache: {
+    cacheLocation: 'localStorage',
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (logLevel: LogLevel, message: string) => {
+        console.log(message);
+      },
+      piiLoggingEnabled: true,
+      logLevel: LogLevel.Verbose,
+    },
+  },
+};
---
+++ apps/porrtal-auth/a-msal-stand-alone-app/src/app/views/v2/v2.component.spec.ts
@@ -0,0 +1,36 @@
+﻿/*
+Copyright 2023 Comcast Cable Communications Management, LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { V2Component } from './v2.component';
+
+describe('V2Component', () => {
+  let component: V2Component;
+  let fixture: ComponentFixture<V2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [V2Component],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(V2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
