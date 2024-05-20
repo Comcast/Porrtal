6af80293b8e2cef0beadad24f2d99cb67429e710
Graham, Mike <Charles_Graham@comcast.com>
2024-05-20 09:55:44 -0600

    feat(workspace): update Copyright to 2024

--- libs/porrtal/r-api/src/lib/porrtal-menu-item.ts
+++ libs/porrtal/r-api/src/lib/porrtal-menu-item.ts
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2023 Comcast Cable Communications Management, LLC
+Copyright 2024 Comcast Cable Communications Management, LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 /*
-Copyright 2023 Comcast Cable Communications Management, LLC
+Copyright 2024 Comcast Cable Communications Management, LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
